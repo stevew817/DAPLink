d3b33e5de531c0e0c6191246e8b8ac0475dd9143
Steven Cooreman <Steven.Cooreman@silabs.com>
2015-09-30 01:29:40 +0200

    Keep keil happy

--- source/common/usb/usbd_core.c
+++ source/common/usb/usbd_core.c
@@ -188,7 +188,7 @@ void USBD_StatusOutStage (void) {
  *    Return Value:    TRUE - Success, FALSE - Error
  */
 
-__inline BOOL USBD_ReqGetStatus (void) {
+BOOL USBD_ReqGetStatus (void) {
   U32 n, m;
 
   switch (USBD_SetupPacket.bmRequestType.Recipient) {
@@ -226,7 +226,7 @@ __inline BOOL USBD_ReqGetStatus (void) {
  *    Return Value:    TRUE - Success, FALSE - Error
  */
 
-__inline BOOL USBD_ReqSetClrFeature (U32 sc) {
+BOOL USBD_ReqSetClrFeature (U32 sc) {
   U32 n, m;
 
   switch (USBD_SetupPacket.bmRequestType.Recipient) {
@@ -300,7 +300,7 @@ __inline BOOL USBD_ReqSetAddress (void) {
  *    Return Value:    TRUE - Success, FALSE - Error
  */
 
-__inline BOOL USBD_ReqGetDescriptor (void) {
+BOOL USBD_ReqGetDescriptor (void) {
   U8  *pD;
   U32  len, n;
 
@@ -429,7 +429,7 @@ __inline BOOL USBD_ReqGetConfiguration (void) {
  *    Return Value:    TRUE - Success, FALSE - Error
  */
 
-__inline BOOL USBD_ReqSetConfiguration (void) {
+BOOL USBD_ReqSetConfiguration (void) {
   USB_CONFIGURATION_DESCRIPTOR *pD;
   U32                           alt = 0;
   U32                           n, m;
@@ -550,7 +550,7 @@ __inline BOOL USBD_ReqGetInterface (void) {
  *    Return Value:    TRUE - Success, FALSE - Error
  */
 
-__inline BOOL USBD_ReqSetInterface (void) {
+BOOL USBD_ReqSetInterface (void) {
   USB_COMMON_DESCRIPTOR *pD;
   U32                    ifn = 0, alt = 0, old = 0, msk = 0;
   U32                    n, m;
